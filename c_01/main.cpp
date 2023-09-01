1099544011d12d8af1d019b70d9c2e5a2a1e12f2
devxiaofan <devxiaofan@163.com>
2023-09-01 15:43:40 +0800

    0901

--- c_01/main.cpp
+++ c_01/main.cpp
@@ -9,15 +9,50 @@
 
 using namespace std;
 
+
+class CCYOU
+{
+private:
+    int x,y;
+    
+public:
+    void Setxy(int a, int b);
+
+    int Getx();
+    
+    
+};
+
+void CCYOU::Setxy(int a, int b)
+{
+    x = a; y = b;
+}
+
+// inline 内联函数，频繁被调用，函数语句少时，可以采用，减少函数被执行次数，提高效率
+inline int CCYOU::Getx()
+{
+    return x;
+}
+
+
+int max(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+
+int max(int a, int b, int c)
+{
+    int t = max(a, b);
+    return max(t, c);
+}
+
 int main()
 {
-    int n = 5;
-    int a[10];
-    for(int i = 0; i < n; i++)
-        cin>>a[i];
-    for(int i = 0; i < n; i++)
-        cout<<a[i]<<"-";
+    string s1 = "hello", s2 = " ", s3 = "ccyou";
     
-    cout<<endl;
+    cout <<s1 + s2 + s3<<endl;
+    return 0;
 }
+
+
 
