0f0f2750f1243a75a67a9a235cc9f77fd9f39fe5
devxiaofan <devxiaofan@163.com>
2023-10-09 16:30:39 +0800

    1009

--- c_01/main.cpp
+++ c_01/main.cpp
@@ -1,87 +1,64 @@
 
 #include <iostream>
-
+#include <cmath>
 using namespace::std;
 
-//1. 分别用字符和ASCII码形式输出整数值65和66.
-//int main()
-//{
-//    int num1(55), num2(56);
-//
-//    cout << "字符型: " << (char)num1 << ' ' << (char)num2 << endl;
-//    cout << "ASSII码：" << num1 << ' ' << num2 << endl;
-//
-//    return 0;
-//}
-
-// 2. 编写一个int型变量分配100个整形空间
-//int main()
-//{
-//    int *p;
-//
-//    p = new int[100];
-//
-//    delete []p;
-//
-//    return 0;
-//}
-//
-
-
-//int main()
-//{
-//    float * p;
-//    float sum = 0;
-//
-//    p=new float[15];
-//    cout << "请输入15个数：";
-//    for (int i = 0; i < 15; i++)
-//    {
-//        cin >> p[i];
-//    }
-//    sort(p, p+15);
-//
-//    for (int i = 0; i < 15; i++)
-//    {
-//        cout << p[i] << ' ';
-//        sum += p[i];
-//    }
-//
-//    cout << endl << "sum is: " << sum << endl;
-//    cout << "min is: " << p[0] << endl;
-//
-//    delete []p;
-//
-//    return 0;
-//}
-
-#include <cmath>
-
-class Point {
+class myComplex
+{
 private:
-    double X,Y;
+    double real, imag;
 public:
-    Point(double xi, double yi){ X = xi; Y = yi; }
-    double GetX() { return X; }
-    double GetY() { return Y; }
-    friend double distances( Point& a, Point& b){
-        double dx = a.X - b.Y;
-        double dy = a.Y - b.Y;
-        return sqrt(dx*dx + dy*dy);
-    };
+    myComplex();
+    myComplex(double, double);
+    void outCom();
+    myComplex operator-(const myComplex &c);
+    friend myComplex operator+(const myComplex &c1, const myComplex &c2);
 };
 
-//double distances(Point& a, Point& b)
-//{
-//    double dx = a.X - b.Y;
-//    double dy = a.Y - b.Y;
-//    return sqrt(dx*dx + dy*dy);
-//}
+myComplex::myComplex()
+{
+    real = 0;
+    imag = 0;
+}
+
+myComplex::myComplex(double r, double i)
+{
+    real = r;
+    imag = i;
+}
+
+void myComplex::outCom()
+{
+    cout << "(" << real << "," << imag << ")";
+}
+
+myComplex myComplex::operator - (const myComplex &c)
+{
+    return myComplex(this -> real - c.real, this -> imag - c.imag);
+}
+
+myComplex operator+(const myComplex &c1, const myComplex &c2)
+{
+    return myComplex(c1.real + c2.real, c1.imag + c2.imag);
+}
 
 int main()
 {
-    Point p1(3.5, 5.5), p2(4.5, 6.5);
-    cout << "distances is: " << distances(p1, p2) << endl;
+    myComplex c1(1, 2), c2(3, 4), res;
+    c1.outCom();
+    cout << "operatou+";
+    c2.outCom();
+    cout << "=";
+    res = c1 + c2;
+    res.outCom();
+    cout << endl;
+    c1.outCom();
+    cout << "operator-";
+    c2.outCom();
+    cout << "=";
+    res = c1 - c2;
+    res.outCom();
+    cout << endl;
     
     return 0;
 }
